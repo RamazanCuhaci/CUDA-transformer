6f5e9e90c5e847ec6279a31122d00515e1e8a6f9
RamazanCuhaci <ramazancuhaci@std.iyte.edu.tr>
2024-09-18 13:59:56 +0300

    Change the includes

--- include/CUDA_ASTConsumer.h
+++ include/CUDA_ASTConsumer.h
@@ -1,8 +1,8 @@
 #ifndef CUDA_AST_CONSUMER_H
 #define CUDA_AST_CONSUMER_H
 
-#include "clang/AST/ASTConsumer.h"
 #include "CUDA_ASTVisitor.h"
+#include "clang/AST/ASTConsumer.h"
 
 
 class CUDA_ASTConsumer : public clang::ASTConsumer
