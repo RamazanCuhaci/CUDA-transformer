e4145c30fff3b216c9e4dd69d8e9f185ad9fad75
RamazanCuhaci <ramazancuhaci@std.iyte.edu.tr>
2024-09-18 13:51:42 +0300

    Add comments for main function

--- src/CUDA-transformer.cpp
+++ src/CUDA-transformer.cpp
@@ -5,25 +5,30 @@
 #include "llvm/Support/CommandLine.h"
            
 
-
+// Declare tool options
 static llvm::cl::OptionCategory MyToolCategory("my-tool options");
-
 llvm::cl::list<int> DoubleIndexes("double-indexes", llvm::cl::desc("Specify the indices of the double variable occurrences to modify"), llvm::cl::value_desc("index"));
 
 
 int main(int argc, const char **argv)
 {
 
-    auto ExpectedParser = clang::tooling::CommonOptionsParser::create(argc, argv, MyToolCategory); // Use the fully qualified name
+    // Create parser for command line options
+    auto ExpectedParser = clang::tooling::CommonOptionsParser::create(argc, argv, MyToolCategory);
+    
     if (!ExpectedParser) 
     {
         llvm::errs() << ExpectedParser.takeError();
         return 1;
     }
 
+    // Get command line options
     clang::tooling::CommonOptionsParser &op = ExpectedParser.get(); // Use the fully qualified name
     clang::tooling::ClangTool Tool(op.getCompilations(), op.getSourcePathList());
 
+    // Create factory for front end actions and run
+    // Front end action class has ASTConsumer and ASTConsumer has ASTVisitor
+    // Front end action is the main entry point for the tool.
     auto Factory = clang::tooling::newFrontendActionFactory<CUDA_FrontendAction>();
     int Result = Tool.run(Factory.get());
     
