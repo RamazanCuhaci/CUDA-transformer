a189fe23dabf3281645b3b3817e317131d868413
RamazanCuhaci <ramazancuhaci@std.iyte.edu.tr>
2024-10-05 15:13:51 +0300

    Change expressions that only inside the kernel function

--- src/CUDA_ASTVisitor.h
+++ src/CUDA_ASTVisitor.h
@@ -12,11 +12,16 @@ class CUDA_ASTVisitor : public clang::RecursiveASTVisitor<CUDA_ASTVisitor>
     clang::ASTContext *context;
     clang::Rewriter &writer;
 
+    // We aim that changed the inside the kernel function(mostly)
+    // So we need the ensure the expressions is inside the kernel function not the host functions
+    bool isVisitorInsideKernel{};
+
   public:
     explicit CUDA_ASTVisitor(clang::ASTContext *context, clang::Rewriter &writer, Expressions &targetExpressions);
 
     bool VisitCUDAKernelCallExpr(clang::CUDAKernelCallExpr *kernelCallEexpr);
     bool VisitCallExpr(clang::CallExpr *callExpr);
+    bool VisitFunctionDecl(clang::FunctionDecl *funcDecl);
 
     Expressions &targetExpressions;
 };
